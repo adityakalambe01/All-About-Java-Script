d9e4b9e8393d3565b3bb57ad5bb90c265fd3b1e8
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-06 12:29:43 +0530

    Conditional Statement

--- 03 Conditional Statements/01 If Statement.js
+++ 03 Conditional Statements/01 If Statement.js
@@ -2,6 +2,11 @@ var age = 18;
 
 var stringAge = "18";
 
+
+var nonNum = "chicken";
+
+var op = nonNum - age; // result is NaN (Not A Number) if we write inside if then the it returns false
+
 if(age == stringAge){
     console.log("Inside if");
 }else{
