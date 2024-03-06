98e3b6fc0d83d8a62399e2e324172ae158e90444
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-07 01:57:52 +0530

    Challenge

---
+++ Challenge/02.js
@@ -0,0 +1,4 @@
+var firstname = "aditya";
+var lastname = "kalambe";
+
+console.log(firstname + " " + lastname);
---
+++ Challenge/03.js
@@ -0,0 +1,5 @@
+var firstname = "aditya";
+var lastname = "kalambe";
+var result = `We know that ${firstname} ${lastname} is very tall`;
+
+console.log(result);
