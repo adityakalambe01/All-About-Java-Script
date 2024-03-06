c7649d504ff0bd1b2e4bb7f8fdede7e57c0a273e
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-06 12:27:55 +0530

    Conditional Statements

---
+++ 03 Conditional Statements/01 If Statement.js
@@ -0,0 +1,15 @@
+var age = 18;
+
+var stringAge = "18";
+
+if(age == stringAge){
+    console.log("Inside if");
+}else{
+    console.log("Inside else");
+}
+
+if(age === stringAge){
+    console.log("Inside if");
+}else{
+    console.log("Inside else");
+}
---
+++ 03 Conditional Statements/02 Else If Statement.js
@@ -0,0 +1,13 @@
+var colors = ["red", "green", "yellow","blue"];
+var selectedColors = colors[2];
+
+if (selectedColors === "red") {
+    console.log("red");
+}
+else if (selectedColors === "green") {
+    console.log("green");
+}else if (selectedColors === "yellow") {
+    console.log("yellow");
+}else{
+    console.log("blue");
+}
