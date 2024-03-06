e45490501afaefca5fee5ee954a09fd3bc6119f0
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-06 12:42:09 +0530

    Conditional Statement

---
+++ 03 Conditional Statements/03 Switch Statement.js
@@ -0,0 +1,19 @@
+var colors = ["red", "green", "yellow", "blue"]
+var selectedColors = colors[8]
+
+switch (selectedColors){
+    case "red":
+        console.log("Red");
+        break;
+    case "green":
+        console.log("Green");
+        break;
+    case "yellow":
+        console.log("Yellow");
+        break;
+    case "blue":
+        console.log("Blue");
+        break;
+    default:
+        console.log("No color selected");
+}
